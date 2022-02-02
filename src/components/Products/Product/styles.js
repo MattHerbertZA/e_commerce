f4f5528d46eb691b96d4eec54a332a68b9cbf52a
Matt Herbert <mclherbert@gmail.com>
2022-02-02 21:11:43 +1200

    Made images bigger

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -2,21 +2,19 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
     root: {
-        maxWidth: '80%',
+        maxWidth: '100%',
     },
     media: {
-        height: 150,
+        height: 250,
         paddingTop: '56.25%', // 16:9
     },
     cardActions: {
         display: 'flex',
         justifyContent: 'flex-end',
-        // marginBottom: '-5%'
     },
     cardContent: {
         display: 'flex',
         justifyContent: 'space-between',
-        paddingBottom: 1000
     },
     iconButton: {
         // display: 'inline-block',
